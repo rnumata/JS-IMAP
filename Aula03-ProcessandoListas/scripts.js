9c77bb6a78fc8c5fab78bb93bde4a06d670e2b6e
Regis Numata <regis.numata@gmail.com>
2022-08-08 10:37:33 -0300

    Update scripts.js

--- Aula03-ProcessandoListas/scripts.js
+++ Aula03-ProcessandoListas/scripts.js
@@ -88,3 +88,84 @@ const listaUnicaDosCantores = listaDosCantores.filter((item, i)=>{
 console.log(listaUnicaDosCantores); 
 
 //5. Gere uma lista contendo a quantidade de álbuns que cada cantor possui
+
+let albuns = [    
+  {
+    nome: "Como estão vocês",
+    cantor: "Titãs",
+    ano: 2003,
+    nota: 7
+  },
+  {
+    nome: "Troco Likes",
+    cantor: "Thiago Iorc",
+    ano: 2015,
+    nota: 4.5
+  },
+  {
+    nome: "Mais",
+    cantor: "Marisa monte",
+    ano: 1991,
+    nota: 8.5
+  },
+  {
+    nome: "A tempestade",
+    cantor: "Legião Urbana",
+    ano: 1996,
+    nota: 9.5
+  },
+  {
+    nome: "Domingo",
+    cantor: "Mamonas",
+    ano: 1995,
+    nota: 7.5
+  },
+  {
+    nome: "Ouro de Minas",
+    cantor: "Roupa Nova",
+    ano: 2001,
+    nota: 8
+  },
+  {
+    nome: "Dois",
+    cantor: "Legião Urbana",
+    ano: 1986,
+    nota: 10
+  },
+  {
+    nome: "Radiola",
+    cantor: "Skank",
+    ano: 2004,
+    nota: 6.5
+  },
+  {
+    nome: "Roupa acústico",
+    cantor: "Roupa Nova",
+    ano: 2004,
+    nota: 9
+  },
+  {
+    nome: "Umbilical",
+    cantor: "Thiago Iorc",
+    ano: 2011,
+    nota: 3.5
+  },
+  {
+    nome: "Barulhinho bom",
+    cantor: "Marisa monte",
+    ano: 1996,
+    nota: 7.5
+  }
+];
+
+const lista = albuns.reduce((obj, album) => {
+  
+  if(obj[album.cantor]){
+    throw("Já existe na lista: " + album.cantor);
+  }
+  
+  obj[album.cantor] = true;
+  
+  return obj;
+  
+}, {});
